03d31c20c01b2a63a255b9486b17d449bec9caba
karlajaramillo <karlav.jaramillo@gmail.com>
2022-02-17 20:41:12 +0100

    Add old version of session config commented

--- config/session.config.js
+++ config/session.config.js
@@ -27,3 +27,29 @@ function sessionConfig(app) {
 }
 
 module.exports = sessionConfig;
+
+
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo");
+
+// function sessionConfig(app) {
+//   const { NODE_ENV, MONGODB_URI, SESSION_SECRET } = process.env;
+//   app.set("trust proxy", 1);
+//   app.use(
+//     session({
+//       secret: SESSION_SECRET,
+//       resave: false,
+//       saveUninitialized: true,
+//       store: MongoStore.create({
+//         mongoUrl: MONGODB_URI,
+//       }),
+//       cookie: {
+//         maxAge: 1000 * 60 * 60 * 24 * 365,
+//         sameSite: false,
+//         secure: NODE_ENV === "production",
+//       },
+//     })
+//   );
+// }
+
+// module.exports = sessionConfig;
