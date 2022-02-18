ae536ff2953eae88701037f44922edb6e8d7df1c
karlajaramillo <karlav.jaramillo@gmail.com>
2022-02-18 08:23:10 +0100

    Change resave and saveUnitialized

--- config/session.config.js
+++ config/session.config.js
@@ -12,12 +12,16 @@ function sessionConfig(app) {
   app.use(
     session({
       secret: SESSION_SECRET,
-      resave: false,
-      saveUninitialized: true,
+      // re save the cookie any time there is a change in it
+      resave: true,
+      // save the cookie till there is something attatched to it
+      saveUninitialized: false,
       store: MongoStore.create({
         mongoUrl: MONGODB_URI,
       }),
       cookie: {
+        // mongoUrl: MONGODB_URL,
+        // time to live, how long we should save this cookie
         maxAge: 1000 * 60 * 60 * 24 * 365,
         sameSite,
         secure: isProduction,
